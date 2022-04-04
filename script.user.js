7265b28b3cf9e0ef7f440d82c5441514ec2731ff
Jonah Henriksson <33059163+JonahPlusPlus@users.noreply.github.com>
2022-04-03 21:51:30 -0400

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -287,7 +287,7 @@ async function getCurrentImageUrl(tag) {
 				}
 			}));
 			ws.send(JSON.stringify({
-				'id': '1',
+				'id': String(2 + parseInt(tag)),
 				'type': 'start',
 				'payload': {
 					'variables': {
